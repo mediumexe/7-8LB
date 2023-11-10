deda9c18c48898520d1ada8ed707df9c25af8bd7
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-11-10 16:05:18 +0300

    Add files via upload

---
+++ 10_7LB.pas
@@ -0,0 +1,11 @@
+﻿program zad10_lab7;
+var s:string;
+a:string;
+b:string;
+begin
+  readln(s,a,b);
+  if(pos('abc',s)=1) then
+    writeln('www')
+    else
+    writeln('zzz');
+end.
---
+++ 2_7LB.pas
@@ -0,0 +1,16 @@
+﻿program zad2;
+var s,d,a:string;
+var z,x,c,b:integer;
+  begin
+    writeln('Введите строку');
+    readln(s);
+    d:=s+',';
+    writeln('Итоговая строка');
+    for c:=1 to 2 do
+    begin
+      write(a);
+    end;
+    write(s);
+    writeln();
+    writeln('Количество символов в строке: ',Length(s));
+  end.
