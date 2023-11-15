25e1c5c0d6fd631efe04ccbf62f603955291f0d3
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-11-15 13:03:23 +0300

    Add files via upload

---
+++ zad15_7lb.pas
@@ -0,0 +1,8 @@
+﻿program zad15_7lb;
+var s:string;
+begin
+  readln(s);
+  write('Введите стоку ');
+  s:=s.Replace('xabc','abc');
+  writeln(s);
+end.
---
+++ zad1_7lb.pas
@@ -0,0 +1,8 @@
+﻿program zad15_7lb;
+var s:string;
+begin
+  readln(s);
+  write('Введите стоку ');
+  s:=s.Replace('Nikolay','Oleg');
+  writeln(s);
+end.
