f2fe69afc1cf1cbb7e8dbb6ca31ec3bcfd8c6897
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-11-30 13:26:11 +0300

    Add files via upload

---
+++ 14lb.pas
@@ -0,0 +1,14 @@
+﻿program zad14;
+var
+  s:string;
+  c:integer;
+begin
+  readln(s);
+  while 'word' in s do
+  begin
+  c:= pos('word', s);
+  Delete (s, c, 4);
+  insert('letter', s, c);
+  end;
+  writeln(s);
+end.
---
+++ 5lb.pas
@@ -0,0 +1,14 @@
+﻿program zad5;
+var
+ s,v:string;
+  c,d,i:integer;
+begin
+  readln(s);
+  c:= Length(s);
+  v:= s[c];
+  for i:=1 to c do
+  begin
+    if v=s[i] then
+    write(' ',i);
+  end;
+end.
